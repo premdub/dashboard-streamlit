7262039fbefef7708fad26228c163012c6d08531
premdub <111820562+premdub@users.noreply.github.com>
2022-11-15 23:39:27 -0500

    update

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from matplotlib.dates import DateFormatter
 import matplotlib.dates as mdates
 import seaborn as sb
 from pandas.api.types import CategoricalDtype
-import matplotlib 
-import inline
+#import matplotlib 
+#import inline
 import splot
 import containers
 import seaborn as sns
